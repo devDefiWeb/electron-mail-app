vladimiry/ElectronMail

commit 64b0c6b5d8227d12dbcf9087b79a8e560778adae

web/angular: prevent extra props to be passed to AngularCompilerPlugin

webpack-configs/web/browser-window.ts
@@ -1,3 +1,4 @@
+import * as ts from 'typescript';
 import {AngularCompilerPlugin, NgToolsLoader, PLATFORM} from "@ngtools/webpack";
 import {DefinePlugin} from "webpack";
 import {readConfiguration} from "@angular/compiler-cli";
@@ -64,20 +65,12 @@ const config = buildBaseWebConfig(
             (() => {
                 type StrictTemplateOptions
                     = NoExtraProperties<Required<import("@angular/compiler-cli/src/ngtsc/core/api").StrictTemplateOptions>>;
-                // enabling the flags via code rather than via the tsconfig.json enforces all the flags to be enabled
-                // so if new flags get added in the "@angular/*" updates then we get compilation error until we add new flags too
-                const strictTemplateOptions: StrictTemplateOptions = {
-                    strictAttributeTypes: true,
-                    strictContextGenerics: true,
-                    strictDomEventTypes: true,
-                    strictDomLocalRefTypes: true,
-                    strictInputTypes: true,
-                    strictLiteralTypes: true,
-                    strictNullInputTypes: true,
-                    strictOutputEventTypes: true,
-                    strictSafeNavigationTypes: true,
-                    strictTemplates: true,
+                const strictTemplateOptions: NoExtraProperties<Pick<StrictTemplateOptions, "strictTemplates">> = {
+                    // if "true", implies all template strictness flags below (unless individually disabled)
+                    // see https://angular.io/guide/template-typecheck
+                    strictTemplates: angularCompilationFlags.ivy,
                 };
+
                 type LegacyNgcOptions
                     = NoExtraProperties<Required<Pick<import("@angular/compiler-cli/src/ngtsc/core/api").LegacyNgcOptions,
                     | "fullTemplateTypeCheck"
@@ -86,24 +79,34 @@ const config = buildBaseWebConfig(
                     fullTemplateTypeCheck: angularCompilationFlags.aot || angularCompilationFlags.ivy,
                     strictInjectionParameters: true,
                 };
-                return new AngularCompilerPlugin({
+
+                type AngularCompilerPluginOptions
+                    = NoExtraProperties<import("@ngtools/webpack/src/interfaces").AngularCompilerPluginOptions>;
+                const compilerOptions: Skip<Required<AngularCompilerPluginOptions>["compilerOptions"],
+                    // skipping raw "string" props
+                    Extract<keyof Required<AngularCompilerPluginOptions>["compilerOptions"], string>> = {
+                    preserveWhitespaces: false,
+                    disableTypeScriptVersionCheck: true,
+                    enableIvy: angularCompilationFlags.ivy,
+                    module: ts.ModuleKind.ESNext,
+                    ...legacyNgcOptions,
+                    ...strictTemplateOptions,
+                    ...readConfiguration(tsConfigFile).options,
+                };
+
+                const angularCompilerPluginOptions: AngularCompilerPluginOptions = {
                     contextElementDependencyConstructor: require("webpack/lib/dependencies/ContextElementDependency"),
                     tsConfigPath: tsConfigFile,
-                    compilerOptions: {
-                        ...strictTemplateOptions,
-                        ...legacyNgcOptions,
-                        preserveWhitespaces: false,
-                        disableTypeScriptVersionCheck: true,
-                        enableIvy: angularCompilationFlags.ivy,
-                        ...readConfiguration(tsConfigFile).options,
-                    },
+                    compilerOptions,
                     platform: PLATFORM.Browser,
                     skipCodeGeneration: !angularCompilationFlags.aot,
                     nameLazyFiles: true,
                     discoverLazyRoutes: true, // TODO disable "discoverLazyRoutes" once switched to Ivy renderer
                     directTemplateLoading: false,
                     entryModule: `${browserWindowAppPath("./app.module")}#AppModule`,
-                });
+                };
+
+                return new AngularCompilerPlugin(angularCompilerPluginOptions);
             })(),
         ],
         optimization: {

devDefiWeb/electron-mail-app

commit 54908d6ab4c4bb60e9320ca2da0b6fb16abf6245

web/angular/webpack: get back splitting to separate/lazy chunks
* broken by 64b0c6b5d8227d12dbcf9087b79a8e560778adae

webpack-configs/web/browser-window.ts
@@ -1,4 +1,3 @@
-import * as ts from 'typescript';
 import {AngularCompilerPlugin, NgToolsLoader, PLATFORM} from "@ngtools/webpack";
 import {DefinePlugin} from "webpack";
 import {readConfiguration} from "@angular/compiler-cli";
@@ -88,7 +87,6 @@ const config = buildBaseWebConfig(
                     preserveWhitespaces: false,
                     disableTypeScriptVersionCheck: true,
                     enableIvy: angularCompilationFlags.ivy,
-                    module: ts.ModuleKind.ESNext,
                     ...legacyNgcOptions,
                     ...strictTemplateOptions,
                     ...readConfiguration(tsConfigFile).options,